a6d396f7454e9cc132e3d9e3ae1d10d84436de1b
shreyasstha <shreya.sstha28@gmail.com>
2025-01-12 19:22:24 +0545

    images

--- src/app.js
+++ src/app.js
@@ -7,24 +7,12 @@ import cartRoutes from "./routes/cart.route.js"
 import orderRoutes from "./routes/order.route.js"
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 
 const app =express();
 
 app.use(express.json());
-app.use(bodyParser.json());
-
 app.use(cors());
 app.use(cookieParser())
-
-
-// app.post('/upload', upload.single('profile'), function (req, res, next) {
-//     console.log("req:", req.file);
-//     // req.file is the `avatar` file
-//     // req.body will hold the text fields, if there were any
-//     res.status(200).json({message:"successful"})
-// })
-// app.use("/image", imageRoutes)
 
 app.get("/", (req,res) =>{ res.json({message: "welcome to backend, Shreya, happy new year 2025"}) })
 app.use("/auth", authRoutes)
