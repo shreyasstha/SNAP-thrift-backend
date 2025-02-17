fba8f0632ffd6d15228bd6da63222cc62fb4bf15
shreyasstha <shreya.sstha28@gmail.com>
2025-02-17 09:57:59 +0545

    cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,11 @@ import khaltiRoutes from "./routes/khalti.route.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000", // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
