94b4fae8c6621dd898a6879d553480ee33bf7eca
shreyasstha <shreya.sstha28@gmail.com>
2025-02-22 12:45:43 +0545

    some changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import userRoutes from "./routes/user.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import orderRoutes from "./routes/order.route.js";
-import packageRoutes from "./routes/package.route.js"
+import packageRoutes from "./routes/package.route.js";
 import khaltiRoutes from "./routes/khalti.route.js";
 import { config } from "dotenv";
 // import paymentRoutes from "./routes/payment.route.js"
@@ -17,20 +17,19 @@ import { config } from "dotenv";
 const app = express();
 config();
 
-
 app.use(express.json());
-app.use(cors(
-  {
-    origin: true, 
+app.use(
+  cors({
+    origin: ["localhost :3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the backend, Shreya. Happy New Year 2025!" });
+  res.json({ message: "Welcome to the backend, Shreya. Happy New Year 2025!" });
 });
 
 // Register routes
@@ -40,7 +39,7 @@ app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
 app.use("/order", orderRoutes);
 app.use("/package", packageRoutes);
-app.use("/khalti", khaltiRoutes); 
+app.use("/khalti", khaltiRoutes);
 
 app.use(errorHandler);
 
